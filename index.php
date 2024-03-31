ae35822b429451fc4c8e5ae0ce388e3a29d7f4f3
witorlinhares <154279407+witorlinhares@users.noreply.github.com>
2024-03-31 10:42:15 -0300

    update

--- index.php
+++ index.php
@@ -1,27 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/style.css">
-    <link rel="icon" href="assets/favicon.png" type="image/x-icon">
-    <title>Sorteador</title>
+    <link rel="icon" href="assets/image.png" type="image/x-icon">
+    <title>Sorteador Online</title>
 </head>
-
-<body>
-    <main>
-    <img src="assets/favicon.png" alt="image">
-        <h1>Sorteador Online</h1>
+<body class="general">
+    <main class="container">
+        <div class="div-image">
+        <img class="image" src="assets/image.png" alt="image">
+        </div>
+        <div class="div-title">
+        <h1 class="title" >Sorteador Online</h1>
+        </div>
         <?php
-        $min = 1;
-        $max = 100;
-        $num = mt_rand($min, $max);
-        echo "<p>Sorteando um número entre <strong>$min</strong> e <strong>$max</strong><br>
-    </p><button>Número sorteado <strong>$num</strong></button>"
+        $min = 1; /*Número mínimo pode ser = ou + que 0*/
+        $max = 1000; /*Número máximo deve ser + que o número mínimo*/
+        $num = mt_rand($min, $max); /*Determina quais números serão serteados*/
+        echo "<h2>Sorteando um número entre <strong>$min</strong> e <strong>$max</strong><br></h2>
+        <p>Número sorteado <strong>''$num''</strong></p>" /*Número sorteado exibido*/
         ?>
-        <div><button onclick="javascript:document.location.reload()">Sortear outro número</button></div>
+        <div class="div-button">
+            <button class="button" onclick="javascript:document.location.reload()">Sortear outro número</button><!--Função que sortea outros números-->
+        </div>
     </main>
 </body>
-
 </html>
+
+<!-- Desenvolvido por Witor Linhares -->
